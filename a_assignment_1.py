5f2c0f360172a8d588aaca9b7fe67d3ca30fb9be
Lochlann O'Regan <27902576+lochlannoregan@users.noreply.github.com>
2021-03-30 10:17:51 +0100

    Added fitness function implementation for evolving a target string

--- a_assignment_1.py
+++ a_assignment_1.py
@@ -1,49 +1,49 @@
 import random
 import matplotlib.pyplot as plt
-import numpy as np
-from matplotlib import colors
-from matplotlib.ticker import PercentFormatter
 
 
-def one_max_problem():
-    print("Starting one max problem")
-    population_size = 100
-    # crossover, mutation and elitism must add to 1.0
-    crossover_percentage = 0.8
-    mutation_percentage = 0.01
-    elitism_percentage = 0.19
+one_max_problem = False
+evolving_target_string = True
+deceptive_landscape_problem = False
 
-    representation_length = 20
-    number_of_individuals_for_crossover = int(population_size * crossover_percentage)
-    number_of_individuals_for_mutation = int(population_size * mutation_percentage)
-    number_of_individuals_for_elitism = int(population_size * elitism_percentage)
-    max_number_bits_flip_mutation = 1
-    k_ways = 4
-    generations = 100
+population_size = 100
+
+# crossover, mutation and elitism must add to 1.0
+crossover_percentage = 0.8
+mutation_percentage = 0.01
+elitism_percentage = 0.19
+
+number_of_individuals_for_crossover = int(population_size * crossover_percentage)
+number_of_individuals_for_mutation = int(population_size * mutation_percentage)
+number_of_individuals_for_elitism = int(population_size * elitism_percentage)
+
+representation_length = 20
+
+max_number_bits_flip_mutation = 1
+k_ways = 4
+generations = 100
+
+target_string_to_evolve = "11110000111100001111"
+
+
+def evolve_ga(graph_title):
     population = []
     average_generation_fitness = []
 
     # Initialise population
-    for i in range(population_size):
-        # getrandbits often gives a values less than the specified number so generating larger bit size and slicing 20
-        representation = str(bin(random.getrandbits(30))[2:22])
-        chromosome = {'representation': representation, 'fitness': fitness_function(representation,
-                                                                                    representation_length)}
-        population.append(chromosome)
+    initialise_population(population)
 
     # Evolve
     for generation in range(generations):
         temp_population = []
         # Apply crossover
-        crossover(temp_population, number_of_individuals_for_crossover, population, population_size, k_ways,
-                  representation_length)
+        crossover(temp_population, population)
 
         # Apply mutation
-        mutation(temp_population, number_of_individuals_for_mutation, population, population_size, k_ways, 
-                 representation_length, max_number_bits_flip_mutation)
+        mutation(temp_population, population)
 
         # Perform elitism to complete population for next generation
-        elitism(temp_population, number_of_individuals_for_elitism, population, population_size, k_ways)
+        elitism(temp_population, population)
 
         # Assemble crossover, mutation and elitism by setting population for next generation
         population = temp_population
@@ -57,7 +57,15 @@ def one_max_problem():
         print("Generation: ", generation, format(average, ".3f"))
 
     # Output matplotlib graph
-    graph_results(average_generation_fitness, generations, "One-Max GA")
+    graph_results(average_generation_fitness, generations, graph_title)
+
+
+def initialise_population(population):
+    for i in range(population_size):
+        # getrandbits often gives a values less than the specified number so generating larger bit size and slicing 20
+        representation = str(bin(random.getrandbits(30))[2:22])
+        chromosome = {'representation': representation, 'fitness': fitness_function(representation)}
+        population.append(chromosome)
 
 
 def graph_results(average_generation_fitness, generations, title):
@@ -72,21 +80,20 @@ def graph_results(average_generation_fitness, generations, title):
     plt.show()
 
 
-def elitism(temp_population, number_of_individuals_for_elitism, population, population_size, k_ways):
+def elitism(temp_population, population):
     for i in range(number_of_individuals_for_elitism):
-        temp_population.append(k_ways_tournament_selection(population, population_size, k_ways))
+        temp_population.append(k_ways_tournament_selection(population, k_ways))
 
 
-def mutation(temp_population, number_of_individuals_for_mutation, population, population_size, k_ways,
-             representation_length, max_number_bits_flip_mutation):
+def mutation(temp_population, population):
     individuals_to_mutate = []
     for i in range(number_of_individuals_for_mutation):
-        individuals_to_mutate.append(k_ways_tournament_selection(population, population_size, k_ways))
+        individuals_to_mutate.append(k_ways_tournament_selection(population, k_ways))
     for individual in individuals_to_mutate:
-        temp_population.append(mutate_bit_string(individual, representation_length, max_number_bits_flip_mutation))
+        temp_population.append(mutate_bit_string(individual))
 
 
-def mutate_bit_string(individual, representation_length, max_number_bits_flip_mutation):
+def mutate_bit_string(individual):
     number_of_bits_to_flip = random.randint(1, max_number_bits_flip_mutation)
     for i in range(number_of_bits_to_flip):
         position_to_flip = random.randint(0, 19)
@@ -97,39 +104,48 @@ def mutate_bit_string(individual, representation_length, max_number_bits_flip_mu
         elif individual['representation'][position_to_flip] == '0':
             individual['representation'] = individual['representation'][:position_to_flip] + "1" + \
                                            individual['representation'][position_to_flip + 1:]
-    individual['fitness'] = fitness_function(individual['representation'], representation_length)
+    individual['fitness'] = fitness_function(individual['representation'])
     return individual
 
 
-def crossover(temp_population, number_of_individuals_for_crossover, population, population_size, k_ways,
-              representation_length):
+def crossover(temp_population, population):
     crossover_parents = []
     for i in range(number_of_individuals_for_crossover):
-        crossover_parents.append(k_ways_tournament_selection(population, population_size, k_ways))
+        crossover_parents.append(k_ways_tournament_selection(population, k_ways))
     index = 1
     while index < len(crossover_parents):
-        crossover_one, crossover_two = crossover_bit_strings(crossover_parents[index - 1], crossover_parents[index],
-                                                             representation_length)
+        crossover_one, crossover_two = crossover_bit_strings(crossover_parents[index - 1], crossover_parents[index])
         temp_population.append(crossover_one)
         temp_population.append(crossover_two)
         index += 2
 
 
-def crossover_bit_strings(individual_one, individual_two, representation_length):
+def crossover_bit_strings(individual_one, individual_two):
     crossover_one_representation = individual_one['representation'][0:10] + individual_two['representation'][10:20]
     crossover_two_representation = individual_two['representation'][0:10] + individual_one['representation'][10:20]
     crossover_one = {'representation': crossover_one_representation, 'fitness': fitness_function(
-        crossover_one_representation, representation_length)}
+        crossover_one_representation)}
     crossover_two = {'representation': crossover_two_representation, 'fitness': fitness_function(
-        crossover_two_representation, representation_length)}
+        crossover_two_representation)}
     return crossover_one, crossover_two
 
 
-def fitness_function(individual, representation_length):
-    return individual.count('1') / representation_length
+def fitness_function(individual):
+    if one_max_problem:
+        return individual.count('1') / representation_length
+    elif evolving_target_string:
+        fitness_to_return = 0
+        for i, allele in enumerate(individual):
+            if allele == target_string_to_evolve[i]:
+                fitness_to_return += 1 / representation_length
+        return fitness_to_return
+    elif deceptive_landscape_problem:
+        print("Deceptive lS")
+    else:
+        print("Fitness function undefined")
 
 
-def k_ways_tournament_selection(population, population_size, k_ways):
+def k_ways_tournament_selection(population, k_ways):
     k_random_indices = [random.randint(0, population_size - 1) for k in range(k_ways)]
     max_value = 0
     location = 0
@@ -143,8 +159,7 @@ def k_ways_tournament_selection(population, population_size, k_ways):
 
 
 def main():
-    print("Starting program")
-    one_max_problem()
+    evolve_ga("Evolving Target String")
 
 
 if __name__ == '__main__':
